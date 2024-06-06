b2a8be2096cf1c383c4a072f4dbee66cf965e53a
Dương Đức Anh <79252211+minh1507@users.noreply.github.com>
2024-06-06 17:25:48 +0700

    Update feature home page admin

--- src/modules/admin/home/home.tsx
+++ src/modules/admin/home/home.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import { Card } from "primereact/card";
+import { InputText } from 'primereact/inputtext';
+
+import "./home.scss";
 
 function Home() {
+  const header = <h3 className="text-center">Login</h3>;
   return (
-    <>
-        <p>hello</p>
-    </>
+    <section className="home-admin-page ">
+      <Card header={header} className="home-admin-page-card">
+        <div className="p-inputgroup flex-1 input">
+          <span className="p-inputgroup-addon">
+            <i className="pi pi-user"></i>
+          </span>
+          <InputText placeholder="Username"/>
+        </div>
+      </Card>
+    </section>
   );
 }
 
