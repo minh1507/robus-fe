c25e105e9902cfcc5d5ac12a8722a9a09f3ff818
Dương Đức Anh <79252211+minh1507@users.noreply.github.com>
2024-06-06 16:42:57 +0700

    Update init component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./modules/portal/home/home";
 import Error from "./components/error/error";
+import HomeAdmin from "./modules/admin/home/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/"  element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="/admin" element={<HomeAdmin />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
---
+++ src/modules/admin/home/home.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Home() {
+  return (
+    <>
+        <p>hello</p>
+    </>
+  );
+}
+
+export default Home;
