6a44ac550dd233090bca1a0eb51dc9a00fb77e4b
Dương Đức Anh <79252211+minh1507@users.noreply.github.com>
2024-06-06 16:28:44 +0700

    Update error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./modules/portal/home/home";
+import Error from "./components/error/error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/error/error.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import './error.scss'
+
+export function Error() {
+    const navigate = useNavigate();
+
+    const click = () => {
+        navigate('/')
+    }
+
+
+  return (
+    <div id="notfound">
+      <div className="notfound">
+        <div className="notfound-404">
+          <h1>
+            4<span></span>4
+          </h1>
+        </div>
+        <h2>Oops! Page Not Be Found</h2>
+        <button onClick={() => click()}>Back to homepage</button>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
