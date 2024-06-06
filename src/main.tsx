33fa2c17adbb6e5451a9bdd3fd5e47d88bc3ee2d
Dương Đức Anh <79252211+minh1507@users.noreply.github.com>
2024-06-06 16:56:02 +0700

    Update all css to scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./App.scss";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "./i18n";
--- src/modules/portal/home/home.tsx
+++ src/modules/portal/home/home.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Home() {
   return (
     <>
-        <p className="mt-2">hello</p>
+        <p className="mt-5">hello</p>
     </>
   );
 }
